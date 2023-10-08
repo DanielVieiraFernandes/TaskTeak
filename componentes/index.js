37259a09df490efa132b927900e55444d686bc16
DanixxB <danivieiiira@gmail.com>
2023-10-08 01:29:10 -0300

    taskteak

--- componentes/index.js
+++ componentes/index.js
@@ -1,48 +1,112 @@
-import { Text, View, Modal, Alert, TouchableOpacity, StyleSheet, Image, TextInput, FlatList } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import {
+  Text,
+  View,
+  Modal,
+  Alert,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  TextInput,
+  FlatList,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform
+} from 'react-native'
 import { StatusBar } from 'expo-status-bar'
-
+import Checkbox from 'expo-checkbox'
+import { Ionicons, AntDesign } from '@expo/vector-icons';
 export default function Index() {
-
   const [modalVisible, setModalVisible] = useState(false)
   const [data, setData] = useState([])
   const [textInput, setTextInput] = useState('')
+  const [colorCheckBox, setcolorCheckBox] = useState({})
 
   const Item = ({ item }) => {
-    return (
  
-       <View style={styles.containerList}>
-        <View style={styles.containList}> 
-        <View style={styles.checkBoxArea}>
-          
-        </View>
-          <Text>{item}</Text>
-          </View> 
-        </View>
+    const removerTarefa = (id) => {
+      let newData = data.filter((val) => val.id !== id)
+      setData(newData)
+    }
+
+
+    const marcarConcluida = (id) => {
+      setcolorCheckBox({ ...colorCheckBox, [id]: true }) 
+      setTimeout(() => {
+        removerTarefa(id)
+      }, 500) 
+    }
+
+   
+    useEffect(() => {
      
+      const timer = setTimeout(() => {
+        setcolorCheckBox({})
+      }, 500)
+
+      return () => clearTimeout(timer)
+    }, [colorCheckBox])
+
+    return (
+      <View style={styles.containerList}>
+        <View style={styles.containList}>
+          <Text>{item.text}</Text>
+          <View
+            style={styles.checkBoxContainer}
+          >
+          <View
+            >
+              <Checkbox
+              value={item.concluido}
+              onValueChange={() => marcarConcluida(item.id)}
+              style={[
+                styles.checkBoxArea,
+                {
+                backgroundColor: colorCheckBox[item.id] ? '#000' : 'transparent',
+                borderRadius: 20,
+                borderColor: 'black',
+                padding: 5
+                },
+                ]}
+              />
+          </View>
+          </View>
+        </View>
+      </View>
     )
   }
 
   return (
-    <View style={styles.container}>
+    <ImageBackground
+    
+    source={require('./img/corno.png')}
+    style={{flex:1, resizeMode: 'cover'}}
+    >
+      <View
+       style={styles.container}>
       <View style={styles.containerHead}>
         <View style={styles.imageContainer}>
-          <Image style={styles.imageContain} source={require('./img/dante-devil-may-cry-capcom-video-games-devil-may-cry-5-hd-wallpaper-preview.jpg')}></Image>
+          <Image style={styles.imageContain} source={require('./img/eu.png')}></Image>
         </View>
-        <View style={styles.perfilContain}>
-          <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#fff' }}>Dante</Text>
-          <Text style={{ fontWeight: '400', fontSize: 12, color: '#fff' }}>dantesparda@gmail</Text>
-        </View>
-        <View style={styles.headerTwo}>
-        <Image style={styles.logo}  source={require('./img/taskteak_logo.png')}></Image>
-        </View>
+       <View style={styles.iconReturn}>
+       <Ionicons name="return-up-back" size={34} color="white" />
+       </View>
       </View>
-       <FlatList
+      <View style={styles.textArea}>
+          <Text style={styles.textTarefas}>Tarefas</Text>
+       </View>
+      <View style={styles.containerMain}>
+      <FlatList
         data={data} 
         renderItem={({ item }) => <Item item={item} />} 
         keyExtractor={(item, index) => index.toString()} 
+        style={{flex: 1}}
       />
       {/* MODAL */}
+      <KeyboardAvoidingView
+       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+       style={styles.container}
+       keyboardVerticalOffset={-100} >
       <Modal
         animationType='slide'
         transparent={true}
@@ -55,29 +119,37 @@ export default function Index() {
        <View style={styles.modalVisible}>
           <View style={styles.containerModal}>
           <View style={styles.headModal}>
-        <View style={{ flex: 1, alignItems: 'center'}}>
-          <Text style={styles.textModal}>Nova Tarefa</Text>
-        </View>
-        <TouchableOpacity style={styles.TouchableOpacityButtonModal} onPress={() => setModalVisible(!modalVisible)}><Text style={styles.buttonClose}>X</Text></TouchableOpacity>
+         <Text style={styles.textModal}>Nova Tarefa</Text>
+      <TouchableOpacity  onPress={() => setModalVisible(!modalVisible)}><Text style={styles.buttonClose}><AntDesign name="closecircle" size={34} color="black" /></Text></TouchableOpacity>
       </View>
+     
             <View style={styles.mainModal}>
             <TextInput placeholder='O que você está planejando?' value={textInput} onChangeText={(text) => setTextInput(text)} style={styles.inputModal}></TextInput>
             </View>
             <TouchableOpacity style={styles.TouchableOpacityButton} onPress={() => {
                   if (textInput.trim() !== '') {
-                    setData([...data, textInput])
+                    const novaTarefa = {
+                      id: Math.random().toString(),
+                      text: textInput,
+                      concluido: false
+                    }
+                    setData([...data, novaTarefa])
                     setTextInput('')
-                    setModalVisible(!modalVisible);
+                    setModalVisible(!modalVisible)
                   }
+                  
                 }}><Text style={styles.textTouchableOpacity}>Criar</Text></TouchableOpacity>
           </View>
-        </View>
+       </View>
       </Modal>
+      </KeyboardAvoidingView>
       <View style={styles.containerButton}>
         <TouchableOpacity onPress={() => setModalVisible(!modalVisible)} style={styles.TouchableOpacityButtonTwo}><Text style={{ fontSize: 50, color: 'white', fontWeight: '100'}}>+</Text></TouchableOpacity>
       </View>
       <StatusBar hidden />
     </View>
+      </View>
+    </ImageBackground>
   )
 }
 
@@ -87,18 +159,17 @@ const styles = StyleSheet.create({
   },
 
   containerHead: {
-    backgroundColor: '#5787fd',
-    flexDirection: 'row',
+    flexDirection: 'row-reverse',
     alignItems: 'center',
     gap: 10,
-    borderBottomEndRadius: 30,
-    borderBottomLeftRadius: 30
+    height: 80,
+    margin: 20,
   },
-   headerTwo: {
+   iconReturn: {
     flex: 1,
-    alignItems: 'flex-end',
-    margin: 20
-  },  
+    justifyContent: 'flex-start',
+    height:'80%'
+   },
 
   containerModal: {
     flex: 0.5,
@@ -108,18 +179,16 @@ const styles = StyleSheet.create({
   },
   modalVisible: {
     flex: 1,
-    justifyContent: 'flex-end'
-  },
-  textModal: {
-    color: '#000',
-    fontSize: 25,
+    justifyContent: 'flex-start'
   },
   TouchableOpacityButton: {
     backgroundColor: '#3d41c1',
     alignItems: 'center',
-    width: '100%',
+    width: '50%',
     height: 50,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    margin: 20,
+    borderRadius: 10
   },
   containerButton: {
     position: 'absolute',
@@ -135,10 +204,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   imageContain: {
-    width: 60,
-    height: 60,
-    marginLeft: 15,
-    borderRadius: 40
+    width: 70,
+    height: 70,
+    borderRadius: 40,
   },
   logo: {
     width: 120,
@@ -151,13 +219,17 @@ const styles = StyleSheet.create({
     width: '90%',
     height: 90,
     borderRadius: 10,
-    backgroundColor: '#e9e9e9',
+    backgroundColor: '#caf0f8',
     elevation: 10,
-    padding: 10
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+
   },
   containerList: {
+    flex: 1,
     alignItems: 'center',
-    padding: 25
+    padding: 20
   },
   inputModal: {
     backgroundColor: 'white',
@@ -180,9 +252,18 @@ const styles = StyleSheet.create({
   headModal: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-between',
-    margin: 10
+    justifyContent: 'center', 
+    margin: 10,
   },
+  textModal: {
+    color: '#000',
+    fontSize: 25,
+    textAlign: 'center',
+    flex: 1,
+  },
+  buttonClose: {
+    alignSelf: 'flex-end', 
+  },  
   mainModal: {
     flex: 1,
     width: '100%',
@@ -193,7 +274,31 @@ const styles = StyleSheet.create({
     color:'#fff',
     fontSize: 18
   },
-  buttonClose:{
-    fontSize: 25
+  checkBox: {
+    width: 25,
+    height: 25,
+    
+    borderRadius: 20
+  },
+  checkBoxContainer: {
+    flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    margin:20,
+  },
+  textTarefas: {
+    color: '#fff',
+    fontSize: 30
+  },
+  textArea: {
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    margin: 20
+  },
+  containerMain: {
+    flex: 1,
+    backgroundColor: 'white',
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
   }
 })
